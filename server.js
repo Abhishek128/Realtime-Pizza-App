4cf9ac7a5228ab2d7b12f0270076e3b8c50c6861
Abhishek <abhishekja.ec18@nsut.ac.in>
2021-07-28 16:25:24 +0530

    make home Page

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req,res) => {
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'./resources/views'))
 app.set('view engine','ejs')
-
+app.use(express.static('public'))
 app.listen(PORT,()=>{
     console.log(`Lisening on Port ${PORT} : http://localhost:3300`)
 })
