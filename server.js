19b3e056a1b9a7648c57bc8121ddee04c436a147
Abhishek <abhishekja.ec18@nsut.ac.in>
2021-07-31 00:40:37 +0530

    add cart page and auth

--- server.js
+++ server.js
@@ -5,13 +5,25 @@ const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3300
 
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'./resources/views'))
+app.set('view engine','ejs')
+
+
 app.get('/', (req,res) => {
     res.render('home')
 })
 
-app.use(expressLayout)
-app.set('views',path.join(__dirname,'./resources/views'))
-app.set('view engine','ejs')
+app.get('/cart',(req,res) =>{
+    res.render('costomer/cart')
+})
+app.get('/login',(req,res) =>{
+    res.render('auth/login')
+})
+app.get('/register',(req,res) =>{
+    res.render('auth/register')
+})
+
 app.use(express.static('public'))
 app.listen(PORT,()=>{
     console.log(`Lisening on Port ${PORT} : http://localhost:3300`)
