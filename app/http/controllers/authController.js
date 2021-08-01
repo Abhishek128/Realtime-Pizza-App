d4da5c5fcb92ee3bc0927cc6af1b55558fc0626d
Abhishek <abhishekja.ec18@nsut.ac.in>
2021-08-01 19:19:09 +0530

    auth System

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -1,12 +1,83 @@
+const User = require('../../models/user')
+const bcryt = require('bcrypt')
+const passport = require('passport')
 function authController(){
     
     return {
         login(req,res){
             return res.render('auth/login')
         },
+        postlogin(req,res,next){
+            const { email, password }   = req.body
+           // Validate request 
+            if(!email || !password) {
+                req.flash('error', 'All fields are required')
+                return res.redirect('/login')
+            }
+            passport.authenticate('local',(err,user,info)=>{
+                console.log(info)
+                console.log(err)
+                if (err) {
+                    console.log(info)
+                    req.flash('error',info.message)
+                    return next(err)
+                }
+                if (!user){
+                    console.log(info)
+                    console.log(err)
+                    console.log(info.message)
+                    req.flash('error',info.message)
+                    return res.redirect('/login')
+                }
+                req.logIn(user,(err)=>{
+                    if (err) {
+                        req.flash('error',info.message)
+                        return next(err)
+                    }
+                    return res.redirect('/')
+                })
+            })(req,res,next)
+        },
         register(req,res){
             return res.render('auth/register')
+        },
+        async postregister(req,res){
+            const {name,email,password} = req.body
+            // console.log(req.body)
+            if (!name || !email || !password){
+                req.flash('error','All Feilds Required')
+                req.flash('name',name)
+                req.flash('email',email)
+                return res.redirect('/register')
+            }
+            User.exists({email : email},(err,result)=>{
+                if (result) {
+                    req.flash('error','Already a registered User')
+                req.flash('name',name)
+                req.flash('email',email)
+                return res.redirect('/register')
+                }
+            })
+            // Hash Password 
+            const hashedPassword = await bcryt.hash(password,10)
+            const user = new User({
+                name : name , 
+                email : email , 
+                password : hashedPassword
+            })
+            
+            user.save().then((user) => {
+                return res.redirect('/')
+            }).catch(err => {
+                req.flash('error','Something went wrong')
+                return res.redirect('/register')
+            })
+        },
+        logout(req,res){
+            req.logout()
+            return res.redirect('/login')
         }
+
     }
 }
 
--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash   = require('express-flash')
 const  MongoDbStore = require('connect-mongo')(session)
-// new MongoDbStore(session)
+const passport = require('passport')
+
 const url = 'mongodb://localhost/pizza'
+
 mongoose.connect(url,{
     useNewUrlParser:true,
     useCreateIndex:true,
@@ -23,6 +25,7 @@ connection.once('open',() => {
 }).catch(err => {
     console.log('Commection Failed')    
 })
+
 
 const mongoStore = new MongoDbStore({
     mongooseConnection :connection , 
@@ -37,7 +40,12 @@ app.use(session({
     // cookie : {maxAge : 1000*15}
     cookie : {maxAge : 1000*60*60*24}
 }))
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
 
+app.use(express.urlencoded({extended : false }))
 app.use(express.json())
 app.use(flash())
 app.use(expressLayout)
@@ -46,6 +54,7 @@ app.set('view engine','ejs')
 
 app.use((req,res,next) =>{
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 } )
 
